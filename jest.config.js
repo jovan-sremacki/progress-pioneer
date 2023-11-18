518e3da112be9459edb288745602cdae79a73c58
Ckojo <jovansr@protonmail.com>
2023-11-18 14:58:23 +0100

    DEV-2 Jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.spec.json'
-    }
-  }
+  testEnvironment: 'node'
 };
